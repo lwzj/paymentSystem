ff75cacef0e40679b1fdf420cb5bfcd02e4685c0
junliang <jun.liang@magicabc.com.cn>
2018-11-24 18:07:42 +0800

    新增自定义CLassLoader

---
+++ src/main/java/com/lwj/paymentsystem/util/RandomUtil.java
@@ -0,0 +1,11 @@
+package com.lwj.paymentsystem.util;
+
+import java.text.DecimalFormat;
+import java.util.Random;
+
+public class RandomUtil {
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.0");
+        System.out.println(df.format(Math.random()*2+5.8));
+    }
+}
